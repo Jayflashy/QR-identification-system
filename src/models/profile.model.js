0781ce0651af609a9bd5af176bdc73348056f1fc
jayflashy <jadesdevelopers@gmail.com>
2022-09-16 21:30:23 +0100

    profile model

---
+++ src/models/profile.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const {Schema, model} = mongoose ;// import Schema & model
+
+// Profile Schema
+const ProfileSchema = new Schema({
+    id: {
+        type: String,
+        required: [true, 'ID is important'],
+        unique:true,
+        trim:true
+    },
+    firstname: {
+        type: String,
+        required: [true, 'A user must have a name'],
+        maxlength: [30, 'firstname must be less than or equal to 30 characters.'], 
+        trim:true
+    },
+    lastname: {
+        type: String,
+        required: [true, 'lastname is required'],
+        maxlength: [30, 'lastname must be less than or equal to 30 characters.'], 
+        trim:true
+    },
+
+},
+{
+    timestamps:{
+        createdAt: 'dateCreated',
+        updatedAt:false
+    }
+})
+
+// Profile model
+const profileModel = model("Profile", ProfileSchema)
+
+module.exports = profileModel
